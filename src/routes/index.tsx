a1e5b79601c5edf1a603e818e37f776a4b536d10
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-09-28 09:12:02 -0400

    Add grid layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,15 +18,21 @@ export default function Home() {
               <h1 class="text-4xl font-bold max-w-96">Hello, I'm Brock.</h1>
               <h3 class="text-xl text-primary">Software Developer, Cybersecurity Enthusiast</h3>
               <p class="mt-4 max-w-3xl">
-                I'm a high school student from Ohio with several years of programming experience,
+                I'm a high school student from Ohio with years of programming experience,
                 focusing on network programming, cryptography, and malware development.
                 Having a diverse skill set in these areas gives me a well-rounded understanding of the cybersecurity landscape.
                 I'm also passionate about bridging the gap between the technical and business aspects of technology,
                 emphasizing clear communication and collaboration to push results.
               </p>
               <div class="flex flex-row mt-8 space-x-4">
-                <Button as={A} href="/resume" size="lg">Resume.md</Button>
-                <Button as={A} href="/#about" variant="outline" size="lg">About Me</Button>
+                <Button as={A} href="/resume" size="lg">
+                  <img src="/icons/markdown.svg" class="size-6 mr-2" />
+                  Resume.md
+                </Button>
+                <Button as={A} href="/#about" variant="outline" size="lg">
+                  <img src="/icons/corner-left-down.svg" class="size-6 mr-2 invert" />
+                  About Me</Button>
+
                 <Button as={A} href="https://github.com/CodedMasonry" variant="outline" size="lg">
                   <img src="/icons/github-mark.svg" class="size-6 mr-2 invert" />
                   Github
@@ -38,7 +44,10 @@ export default function Home() {
       </div>
       <div class="h-screen pt-48 px-40">
         <h2 id="about" class="font-bold text-3xl">Who Am I?</h2>
-        
+        <div class="grid p-4 grid-cols-4 grid-rows-4 gap-4">
+          <div class="border ">Certified Drone Pilot</div>
+          <div class="border">Risk At Rest: How is the US Government protecting ciitizens data?</div>
+        </div>
       </div>
     </main>
   );
