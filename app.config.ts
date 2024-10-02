33775af81f4fc4f2b9de7a700c7c9bf30c7cba8e
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-10-02 13:50:22 -0400

    feat: add prerender for home page

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   server: {
     preset: "vercel",
+    prerender: {
+      routes: ["/"]
+    }
   },
   ssr: true,
   vite: {
