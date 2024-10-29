25f7a7258558b8a4387005adf4f867047de121a1
CodedMasonry <109301662+CodedMasonry@users.noreply.github.com>
2024-10-28 22:10:09 -0400

    add placeholder pages

---
+++ src/routes/risk-at-rest.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main class="text-center space-y-4">
+            <h1 class="mt-40 text-4xl font-bold">Not finished writing it yet, but it will be posted here when it is.</h1>
+        </main>
+    );
+}
